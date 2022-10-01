d22382062441223ea4231214bcf655050fb8a92c
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2022-10-01 16:12:51 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react'
+import firebase from './config'
 
-function App() {
+
+const App = () => {
+ const numberRef = useRef();
+ const codeRef = useRef()
+ const configureCaptcha = () =>{
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+      'size': 'invisible',
+      'callback': (response) => {
+        // reCAPTCHA solved, allow signInWithPhoneNumber.
+        this.onSignInSubmit();
+        console.log("Recaptca varified")
+      },
+      defaultCountry: "IN"
+    });
+  }
+ const onSignInSubmit = (e) => {
+    e.preventDefault()
+    configureCaptcha()
+    const phoneNumber = "+919145298086"
+    
+    const appVerifier = window.recaptchaVerifier;
+    firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+        .then((confirmationResult) => {
+         
+          window.confirmationResult = confirmationResult;
+          console.log("OTP has been sent")
+          // ...
+        }).catch((error) => {
+          // Error; SMS not sent
+          // ...
+          console.log("SMS not sent")
+        });
+  }
+const  onSubmitOTP = (e) =>{
+    e.preventDefault()
+    const code = "685786"
+    console.log(code)
+    window.confirmationResult.confirm(code).then((result) => {
+      // User signed in successfully.
+      const user = result.user;
+      console.log(JSON.stringify(user))
+      alert("User is verified")
+      // ...
+    }).catch((error) => {
+      // User couldn't sign in (bad verification code?)
+      // ...
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   
+       <div>
+        <h2>Login Form</h2>
+        <form onSubmit={onSignInSubmit}>
+          <div id="sign-in-button"></div>
+          <input type="number" name="mobile" placeholder="Mobile number" required ref={numberRef} />
+          <button type="submit">Submit</button>
+        </form>
+
+        <h2>Enter OTP</h2>
+        <form onSubmit={onSubmitOTP}>
+          <input type="number" name="otp" placeholder="OTP Number" required ref={codeRef} />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+ 
+  )
 }
 
-export default App;
+export default App
